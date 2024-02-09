2f038db43d4ee7a5f946aa3ac9f9c897864a7689
yash1314 <yashkeshari79@gmail.com>
2024-02-09 13:56:42 +0530

    app.py changes

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import streamlit as st
 st.title(":rainbow[JSON Viewer]")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 
-st.markdown("""You can now view your Json Data Format with ease. Just Copy your Json Data in Side text
-and get your formatted json data.""")
+st.markdown("""You can now view your Json Data Format with ease. Just Copy your Json Data in Side bar
+and get your JSON Data Format.""")
 st.markdown('------------------------------------------')
 
-user_input = st.sidebar.text_area('Enter your data here')
+user_input = st.sidebar.text_area('**Enter your data here**')
 
-button = st.sidebar.button('Format')
+button = st.sidebar.button('***View***')
 
 if button:
     if user_input[:] == "":

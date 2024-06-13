eb3f403936ee6024fd08161a5758cad9f2a6b7ba
Yash Keshari <128951942+yash1314@users.noreply.github.com>
2024-06-13 17:23:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title(":rainbow[JSON Viewer]")
-st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
+st.markdown('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
 
 st.markdown("""You can now view your Json Data Format with ease. Just Copy your Json Data in Side bar
 and get your JSON Data Format.""")
